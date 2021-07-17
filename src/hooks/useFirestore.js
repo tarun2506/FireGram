2b02aa751e1664076fe8a4ffa8d8cc306468cf3c
tarun2506 <tarunsharma8920@gmail.com>
2021-07-18 01:37:47 +0530

    added imageGrid component and made it working well

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { projectFirestore } from "../firebase/config";
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection(collection)
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        let documents = [];
+        snapshot.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(documents);
+      });
+    return () => unsub();
+  }, [collection]);
+
+  return { docs };
+};
+
+export default useFirestore;
